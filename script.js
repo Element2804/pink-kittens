82dbda501c441e026933129bb68fd701639cc448
Element2804 <103654389+Element2804@users.noreply.github.com>
2023-03-11 23:10:09 -0500

    added character arrays to variables

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 // Assignment code here
+var specialChar = ["@", "%", "+", "/", "'", "!", "#", "$", "^", "?", ":", ",", ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
 function generatePassword(){
     console.log("button test passed");
+   var selectPass = prompt("select the length of your password");
+   console.log(window)
+  // if (selectPass < 8 || > 128)
+
+//prompt the user for the password criteria
+// a.password length between 8-128 
+//  b.lowercase, uppercase, numbers, special characters
+// validate the input
+//generate password based on criteria
 
 
-
-
+// Display password
     return "Password here"
 }
 
