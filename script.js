0f5f8ecd357e7b347b907a3496a164e503b648b8
Element2804 <103654389+Element2804@users.noreply.github.com>
2023-03-12 12:58:28 -0400

    Connected prompt function

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 // Assignment code here
+
+//empty array and mininmum password length
 var userChoice = [];
 var passwordLength = 8;
+
+//array containing possible characters
 var specialChar = ["@", "%", "+", "/", "'", "!", "#", "$", "^", "?", ":", ",", ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-function generatePassword(){
-    console.log("button test passed");
-   var selectPass = prompt("select the length of your password");
-   console.log(window)
-
-
-//prompt the user for the password criteria
-// a.password length between 8-128 
-//  b.lowercase, uppercase, numbers, special characters
-// validate the input
-//generate password based on criteria
+function generatePassword() {
+    var password = "";
+    
+    //calculates password based on the contents of the array and the user's choices
+    for(var i = 0; i <passwordLength; i++) {
+        var randomPass = Math.floor(Math.random() * userChoice.length);
+        password = password + userChoice[randomPass];
+    }
+    console.log("Button test passed");
 
 
 // Display password
-    return "Password here"
+    return password;
 }
 
 // Function containing the password prompts
@@ -50,6 +52,8 @@ passwordLength = parseInt(prompt("Please select a number between 8 - 128"));
         userChoice = userChoice.concat(upperCase);
     }
     return true;
+
+
 }
 
 // Get references to the #generate element
@@ -57,7 +61,9 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+ //calls the prompts from the function
     passPrompt();
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
