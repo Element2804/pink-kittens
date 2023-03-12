163d39055482cc4119e4e8789ea34a6727823b10
Element2804 <103654389+Element2804@users.noreply.github.com>
2023-03-12 13:36:11 -0400

    added comments and fixed merge conflict

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Assignment code here
 var userChoice = [];
+// minimum length of password
 var passwordLength = 8;
+// array with all possible characters
 var specialChar = ["@", "%", "+", "/", "'", "!", "#", "$", "^", "?", ":", ",", ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
@@ -21,7 +23,8 @@ function generatePassword(){
 
 // Function containing the password prompts
 function passPrompt () {
-userChoice = [];
+// resets array between password attempts
+    userChoice = [];
 
 passwordLength = parseInt(prompt("Please select a number between 8 - 128"));
 
